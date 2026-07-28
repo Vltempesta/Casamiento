6fb1aad1c07d419ca818ac0a60be7c934cebeafb
Vltempesta <89156988+Vltempesta@users.noreply.github.com>
2026-07-27 22:01:56 -0300

    config.js

--- app-v2/config.js
+++ app-v2/config.js
@@ -3,7 +3,7 @@
   El token debe coincidir con PUBLIC_WRITE_TOKEN en Apps Script.
 */
 window.WEDDING_APP_CONFIG = {
-  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwQ4g5Dd0mIuY5pU8EsWlYmSjg2CywJwRJkNaT8ipvvGQAnLwcZkzfjMzYq0cFBmIaW/exec",
+  GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxETImB6JB0Cy3UF5-Jppq1MsHzHkOLAIRr7GGp9Vj42tgGKvU7liIgvNJdZ6E7NmN2/exec",
   PUBLIC_WRITE_TOKEN: "VF-2026-BOSQUE",
   LOCAL_ADMIN_PASSWORD: "vanifyfede2026",
   ENABLE_REMOTE_SYNC: true,
